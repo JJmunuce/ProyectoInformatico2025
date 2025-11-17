60b458385a82596d02551a871510535483949041
JJmunuce <jjmunuce@gmail.com>
2025-11-17 20:10:29 -0300

    Se agrego función login en frontend.

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -23,3 +23,66 @@ document.addEventListener('DOMContentLoaded', () => {
         title.textContent = 'Iniciar Sesión'; 
     }
 });
+
+function login(event) {
+    
+    //Evito que se recargue la página
+    event.preventDefault();
+
+    // Obtener valores de usuario y contraseña
+    var username = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    // URL de la API para iniciar sesión
+    var apiUrl = 'http://127.0.0.1:5000/login';
+
+    // Datos de la solicitud
+    var data = {
+        username: email,
+        password: password
+    };
+
+    // Configuración de la solicitud
+    var requestOptions = {
+        method: 'POST',
+        headers: {
+            // Aquí se agrega otro contenido de acuerdo a la necesidad de la API
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' +  btoa(username + ':' + password)
+        },
+        body: JSON.stringify(data)
+    };
+
+    function handleResponse(response)  {
+        if (!response.ok){
+            return Promise.reject(response);
+        }
+        else{
+            return response.json();
+        }
+    }
+
+    // Hacer la solicitud fetch y guardo en el sessionStorage
+    fetch(apiUrl, requestOptions)
+        .then(response => handleResponse(response))
+        .then(userData => {
+            sessionStorage.setItem("id_usuario", userData.id_usuario);
+            sessionStorage.setItem("nombre", userData.nombre);
+            sessionStorage.setItem("apellido", userData.apellido);
+            sessionStorage.setItem("token", userData.token);
+
+            window.location.href = "dashboard.html";
+
+        })
+        .catch(error => {
+            error.json().then(data => 
+                Swal.fire({
+                    icon: "error",
+                    text: data.message
+                  })
+            );
+        })
+        .finally( () => { 
+            console.log("Promesa finalizada (resuelta o rechazada)");
+        })
+};
